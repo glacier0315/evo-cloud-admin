949eba7435c2c62334bf5f2283877d530fd86763
glacier <glacier0315@126.com>
2020-08-07 11:25:48 +0800

    添加代码生成模块

---
+++ modules/generator/src/main/java/com/glacier/generator/GeneratorApplication.java
@@ -0,0 +1,14 @@
+package com.glacier.generator;
+
+/**
+ * @author glacier
+ * @version 1.0
+ * @date 2020-08-07 11:14
+ */
+@SpringBootApplication
+public class GeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratorApplication.class, args);
+    }
+}
